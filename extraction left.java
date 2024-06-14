0087aa36484f3dc99f8419f533bfd8839fc16a2b
hesventhkumar <141727454+hesventhkumar@users.noreply.github.com>
2024-06-14 10:43:14 +0530

    Create extraction left.java

---
+++ extraction left.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+   long num,pv=1,dig;
+        num = sc.nextLong();
+        while(num/pv!=0){
+            pv=pv*10;
+        }
+        pv=pv/10;
+        System.out.println(pv);     //to get pv value
+        while(pv!=0){
+            dig=(num/pv)%10;
+            System.out.printf("%d %d\n",num,dig);
+            pv=pv/10;
+
+        }
+
+
+
+    }
+}
